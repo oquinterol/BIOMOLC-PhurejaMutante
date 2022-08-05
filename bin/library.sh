cbc9dbc95abffa7e1c3d9ec7e8d0cf54e0ef99a3
Alexis Quintero <oaquinterol@correo.udistrital.edu.co>
2022-08-05 12:31:57 -0500

    listo el modulo sra2fq

--- bin/library.sh
+++ bin/library.sh
@@ -3,6 +3,8 @@
 # Primero necesito conda, y que con conda puedo instalar las herramientas de SRA
 # Primero herramienta de paralelizacion de fastq-dump
 # https://github.com/rvalieris/parallel-fastq-dump
-# Tambien instala  sra desde bioconda
+# Tambien instala  sra desde conda
 conda install -c bioconda parallel-fastq-dump
+## Preferiblemente usar pip los pinches PATH son un estress
+python3 -m pip install --user --upgrade cutadapt
 conda install cutadapt
