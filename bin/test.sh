dc63851765f31ac4a8c46ebda175f092726dce8f
Alexis Quintero <oaquinterol@correo.udistrital.edu.co>
2022-08-05 11:37:04 -0500

    listo el modulo sra2fq

--- bin/test.sh
+++ bin/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-DIR=$(pwd)
+DIR=$(pwd)/data
 THD=$(($(nproc --all)-2))
 RAM=$(($(awk '/MemTotal/ { printf "%.0f \n", $2/1024/1024 }' /proc/meminfo)-2))G
 #Impresión de lo basico, para saber información basica del sistema.
 echo 'Directorio: '$DIR
-echo $DIR/data/fastq
+echo 'Prueba agregando mas texto: '$DIR/fastq
 echo '# de Nucleos: '$THD
 echo 'Max. de RAM: '$RAM
